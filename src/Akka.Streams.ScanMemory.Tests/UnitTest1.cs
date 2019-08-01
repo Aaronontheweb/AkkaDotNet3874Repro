4ef9c0a6fc89d9780e52301125f5f5a0de7d6044
Aaron Stannard <aaron@petabridge.com>
2019-08-01 12:56:52 -0500

    initial commit

---
+++ src/Akka.Streams.ScanMemory.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Akka.Streams.ScanMemory.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/Akka.Streams.ScanMemory/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Akka.Streams.ScanMemory
+{
+    public class Class1
+    {
+    }
+}
